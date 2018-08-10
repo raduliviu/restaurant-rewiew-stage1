b3274e5124d6059865f10c172f988dddaa139376
Radu Baltag <baltag.radu@gmail.com>
2018-08-10 14:24:28 +0200

    feat: visited pages cached for offline view

--- sw.js
+++ sw.js
@@ -20,67 +20,41 @@ var cacheFiles = [
     './img/10.jpg',
     './js/dbhelper.js',
     './js/main.js',
-    './js/restaurant_info.js',
+    './js/restaurant_info.js'
 ];
 
-//Here I am creating the cache and adding the files to the cache
-self.addEventListener('install', function(event) {
-    console.log('Attempting to install service worker and cache static assets');
-    event.waitUntil(
-      caches.open(restaurantsCache)
-      .then(function(cache) {
-        return cache.addAll(cacheFiles);
-      })
-    );
-  });
-
-self.addEventListener('fetch', function(event) {
-console.log('Fetch event for ', event.request.url);
-event.respondWith(
-//Here we check if we already have cached the requested resource
-//If we do, we return it, if we do not, then we request it from the network
-    caches.match(event.request).then(function(response) {
-    if (response) {
-        console.log('Found ', event.request.url, ' in cache');
-        return response;
-    }
-    console.log('Network request for ', event.request.url);
-    return fetch(event.request)
-    
-    //Here we are putting returned responses into the cache
-    .then(function(response) {
-        
-        return caches.open(restaurantsCache).then(function(cache) {
-            if (event.request.url.indexOf('test') < 0) {
-            cache.put(event.request.url, response.clone());
-            }
-            return response;
-        });
-        });
-
-    }).catch(function(error) {
-
-    console.log(error);
-
-    })
-);
+self.addEventListener('install', function(evt) {
+    console.log('The service worker is being installed.');
+    evt.waitUntil(precache());
 });
 
-//Here we delete the outdated cache
-self.addEventListener('activate', function(event) {
-    console.log('Activating new service worker...');
-  
-    var cacheWhitelist = [restaurantsCache];
-  
-    event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-        return Promise.all(
-          cacheNames.map(function(cacheName) {
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
-    );
-  });
+self.addEventListener('fetch', function(evt) {
+    console.log('The service worker is serving the asset.');
+    evt.respondWith(fromNetwork(evt.request, 400).catch(function () {
+        return fromCache(evt.request);
+    }));
+});
+
+function precache() {
+    return caches.open(restaurantsCache).then(function (cache) {
+      return cache.addAll(cacheFiles);
+    });
+};
+
+function fromNetwork(request, timeout) {
+    return new Promise(function (fulfill, reject) {
+        var timeoutId = setTimeout(reject, timeout);
+        fetch(request).then(function (response) {
+            clearTimeout(timeoutId);
+            fulfill(response);
+        }, reject);
+    });
+};
+
+function fromCache(request) {
+    return caches.open(restaurantsCache).then(function (cache) {
+        return cache.match(request).then(function (matching) {
+        return matching || Promise.reject('no-match');
+        });
+    });
+};
